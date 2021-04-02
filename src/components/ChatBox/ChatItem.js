9050d2c472d5157cfee91c3be9b995dedb3e62fa
jeonghun-project <dsjoh111@gmail.com>
2021-04-02 12:21:51 +0900

    fix bug chatinput blur add isComposing

--- src/components/ChatBox/ChatItem.js
+++ src/components/ChatBox/ChatItem.js
@@ -1,22 +1,22 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react';
 
-const ChatItem = ({username, message}) => {
-  const newItem = useRef(null)
+const ChatItem = ({ username, message }) => {
+  const newItem = useRef(null);
 
   const scrollToBottom = () => {
-    newItem.current.scrollIntoView()
-  }
+    newItem.current.scrollIntoView();
+  };
 
   useEffect(() => {
-    scrollToBottom()
-  })
+    scrollToBottom();
+  });
 
   return (
     <div ref={newItem} className="ChatItemContainer">
-      {username ? <span className="ChatName">{username} : </span> : '' }
-      {message ? <span className='ChatMsg'>{message}</span> : ''}
+      {username ? <span className="ChatName">{username} : </span> : ''}
+      {message ? <span className="ChatMsg">{message}</span> : ''}
     </div>
-  )
-}
+  );
+};
 
-export default ChatItem
+export default ChatItem;
--- src/components/ChatBox/ChatTooltip.js
+++ src/components/ChatBox/ChatTooltip.js
@@ -1,36 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import styled from "styled-components"
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 const Div = styled.div`
   left: ${props => props.x};
   top: ${props => props.y};
 `;
 
-export default function ChatTooltip({user}) {
-  const [isVisible, setIsVisible] = useState({state:false, timeOutID: null});
-  useEffect(()=>{
-    setIsVisible((state) => {
-      if(state.timeOutID) {
+export default function ChatTooltip({ user }) {
+  const [isVisible, setIsVisible] = useState({ state: false, timeOutID: null });
+  useEffect(() => {
+    setIsVisible(state => {
+      if (state.timeOutID) {
         clearTimeout(state.timeOutID);
       }
-      return ({
+      return {
         ...state,
         state: true,
-        timeOutID : setTimeout(() => {
-          console.log('call timeout')
+        timeOutID: setTimeout(() => {
+          console.log('call timeout');
           setIsVisible({
-            state :false,
+            state: false,
             timeOutID: null,
-          })
-        }, 3000)
-      })
+          });
+        }, 3000),
+      };
     });
-  },[user.message])
-  if (!isVisible.state) return ''
+  }, [user.message]);
+  if (!isVisible.state) return '';
   return (
-    <Div className="chatTooltip" x={user.position.x - 60 + 'px'} y={user.position.y - 70 + 'px'}>
+    <Div
+      className="chatTooltip"
+      x={user.position.x - 60 + 'px'}
+      y={user.position.y - 70 + 'px'}
+    >
       <div className="chatTooltipText">{user.message}</div>
-      <Div className="triangleToolTip" x={70 + 'px'} y={ 45+ 'px'} />
+      <Div className="triangleToolTip" x={70 + 'px'} y={45.5 + 'px'} />
     </Div>
-  )
+  );
 }
